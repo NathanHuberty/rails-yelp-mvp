f32acabd385caea4097217d6875541f2e583d455
Nathan Huberty <nathan.huberty@orange.fr>
2017-11-08 17:00:10 +0100

    brabra

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @review = Review.new
+  end
 
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    return render :new unless @review.save
+    redirect_to @restaurant
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant1 = Restaurant.new({name: "Oh Chi Min", address: "rue du nem", category: "chinese"})
+restaurant2 = Restaurant.new({name: "Tutti Pasti", address: "rue du rital", category: "italian"})
+restaurant3 = Restaurant.new({name: "Sushi", address: "rue du jap", category: "japanese"})
+restaurant4 = Restaurant.new({name: "Baguette", address: "rue du chieur", category: "french"})
+restaurant5 = Restaurant.new({name: "Choux de Brux", address: "rue de la frite", category: "belgian"})
+
+restaurant1.save!
+restaurant2.save!
+restaurant3.save!
+restaurant4.save!
+restaurant5.save!
